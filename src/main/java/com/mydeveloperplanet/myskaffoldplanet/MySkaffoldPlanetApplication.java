b766365e8100a60e34dd864e23cd640740da354d
mydeveloperplanet <gunter@mydeveloperplanet.com>
2019-12-30 10:34:45 +0100

    Initial commit

---
+++ src/main/java/com/mydeveloperplanet/myskaffoldplanet/MySkaffoldPlanetApplication.java
@@ -0,0 +1,13 @@
+package com.mydeveloperplanet.myskaffoldplanet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySkaffoldPlanetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySkaffoldPlanetApplication.class, args);
+    }
+
+}
